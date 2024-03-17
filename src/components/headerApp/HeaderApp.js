6879b6957add3afcf1854bcb328c6b77b9fdb765
Victor Volkov <vityan2504@gmail.com>
2024-03-17 11:21:53 +0300

    .

--- src/components/headerApp/HeaderApp.js
+++ src/components/headerApp/HeaderApp.js
@@ -4,19 +4,19 @@ const HeaderApp = () => {
    return (
       <header className="app__header">
             <h1 className="app__title">
-                <Link to="/">
-                    <span>Marvel</span>
-                    information portal
-                </Link>
-            </h1>
-            <nav className="app__menu">
-                <ul>
-                    <li><NavLink to="/" style={({ isActive }) => ({ color: isActive ? ' #9F0013' : 'inherit' })}>Characters</NavLink></li>
-                    /
-                    <li><NavLink to="/comics" style={({ isActive }) => ({ color: isActive ? ' #9F0013' : 'inherit' })}>Comics</NavLink></li>
-                </ul>
-            </nav>
-        </header>
+               <Link to="/">
+                  <span>Marvel</span>
+                  information portal
+               </Link>
+         </h1>
+         <nav className="app__menu">
+               <ul>
+                  <li><NavLink to="/" style={({ isActive }) => ({ color: isActive ? ' #9F0013' : 'inherit' })}>Characters</NavLink></li>
+                  /
+                  <li><NavLink to="/comics" style={({ isActive }) => ({ color: isActive ? ' #9F0013' : 'inherit' })}>Comics</NavLink></li>
+               </ul>
+         </nav>
+      </header>
    )
 }
 
